3c9acadb7ba081596427d178b80076290a00a866
AnaSofiaDelaCruz <43657612+AnaSofiaDelaCruz@users.noreply.github.com>
2025-02-17 19:06:46 -0600

    Amigo secreto
    Proyecto challenge Amigo secreto. 
    Practica Lógica de Programación.

---
+++ Challenge_Amigo_Secreto/app.js
@@ -0,0 +1,45 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+    
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+    
+    if (!/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/.test(nombre)) {
+        alert("El nombre no puede contener números ni caracteres especiales.");
+        return;
+    }
+    
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
+
+function actualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    
+    amigos.forEach(amigo => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Debe agregar al menos un amigo antes de sortear.");
+        return;
+    }
+    
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSeleccionado = amigos[indiceAleatorio];
+    
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>El amigo secreto es: <strong>${amigoSeleccionado}</strong></li>`;
+}
